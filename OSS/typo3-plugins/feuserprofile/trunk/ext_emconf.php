98d647c9fbb0cac42867b47fdaea22a343fee910
ralph <ralph@fdcf5c44-6d3c-11dc-8539-f79c8e7a170c>
2013-09-05 14:42:14 +0000

    Typo3 6.1.4 compatible versions

---
+++ OSS/typo3-plugins/feuserprofile/trunk/ext_emconf.php
@@ -0,0 +1,45 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "feuserprofile".
+#
+# Auto generated 24-03-2010 21:36
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'RS Frontend User Profile',
+	'description' => 'Flexible Extension to view, list and edit Frontend user profiles',
+	'category' => 'plugin',
+	'author' => 'Ralph Schuster',
+	'author_email' => 'typo3@ralph-schuster.eu',
+	'shy' => '',
+	'dependencies' => 'rsextbase',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '1.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'rsextbase' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"01ff";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"4b34";s:14:"ext_tables.php";s:4:"4565";s:13:"locallang.xml";s:4:"b58e";s:16:"locallang_db.xml";s:4:"82f2";s:19:"doc/wizard_form.dat";s:4:"a8f7";s:20:"doc/wizard_form.html";s:4:"9505";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:34:"pi1/class.tx_feuserprofile_pi1.php";s:4:"a5d5";s:42:"pi1/class.tx_feuserprofile_pi1_wizicon.php";s:4:"daa3";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"e77c";s:36:"static/fe_user_profile/constants.txt";s:4:"4561";s:32:"static/fe_user_profile/setup.txt";s:4:"27d2";}',
+);
+
+?>
